51babe1f6f4c6f5938627522e2e3e49811def6b4
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2023-01-10 11:10:39 +0100

    ✨ TD03 - Bonus

--- TD02/Exo3/index.js
+++ TD02/Exo3/index.js
@@ -55,15 +55,15 @@ fetch(
 // Bonus - Trajet avec MapBox
 //
 
-const pathMap = L.map("pathMap")
-const pathLayer = new L.StamenTileLayer("terrain")
-pathMap.addLayer(pathLayer)
+const pathMapBox = L.map("pathMapBox")
+const pathMapBoxLayer = new L.StamenTileLayer("terrain")
+pathMapBox.addLayer(pathMapBoxLayer)
 
-document.getElementById("coordinates").addEventListener("submit", (e) => {
+document.getElementById("coordinatesMapBox").addEventListener("submit", (e) => {
   e.preventDefault()
-  const startingPoint = document.getElementById("start").value
-  const [startLon, startLat] = startingPoint.split(",")
-  const endPoint = document.getElementById("end").value
+  const startPoint = document.getElementById("startPoint").value
+  const [startLon, startLat] = startPoint.split(",")
+  const endPoint = document.getElementById("endPoint").value
   const [endLon, endLat] = endPoint.split(",")
 
   fetch(
@@ -80,12 +80,67 @@ document.getElementById("coordinates").addEventListener("submit", (e) => {
         latlngs.push([coord[1], coord[0]])
       })
 
-      pathMap.setView([startLat, startLon], 13)
+      pathMapBox.setView([startLat, startLon], 13)
 
-      L.marker([startLat, startLon]).addTo(pathMap).bindPopup(`Départ - ${waypoints[0].name}`)
-      L.marker([endLat, endLon]).addTo(pathMap).bindPopup(`End - ${waypoints[1].name}`)
+      L.marker([startLat, startLon])
+        .addTo(pathMapBox)
+        .bindPopup(`Départ - ${waypoints[0].name}`)
+      L.marker([endLat, endLon])
+        .addTo(pathMapBox)
+        .bindPopup(`End - ${waypoints[1].name}`)
 
-      L.polyline(latlngs, { color: "red" }).addTo(pathMap)
+      L.polyline(latlngs, { color: "red" }).addTo(pathMapBox)
     })
     .catch((e) => console.error(e))
 })
+
+//
+// Bonus - Trajet avec MapQuest
+//
+
+const pathMapQuest = L.map("pathMapQuest")
+const pathMapQuestLayer = new L.StamenTileLayer("terrain")
+pathMapQuest.addLayer(pathMapQuestLayer)
+
+document
+  .getElementById("coordinatesMapQuest")
+  .addEventListener("submit", (e) => {
+    e.preventDefault()
+    const startAddress = document.getElementById("startAddress").value
+    const endAddress = document.getElementById("endAddress").value
+
+    fetch(
+      `http://www.mapquestapi.com/directions/v2/route?key=tR2C6osuQcc3RoWnxDMXF6FACtNAzMl8&from=${startAddress}&to=${endAddress}`
+    )
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+
+        const { locations } = data.route
+        const { maneuvers } = data.route.legs[0]
+        const latlngs = []
+
+        Object.values(maneuvers).forEach((maneuver) => {
+          latlngs.push([maneuver.startPoint.lat, maneuver.startPoint.lng])
+        })
+
+        pathMapQuest.setView(
+          [locations[0].latLng.lat, locations[0].latLng.lng],
+          13
+        )
+
+        L.marker([locations[0].latLng.lat, locations[0].latLng.lng])
+          .addTo(pathMapQuest)
+          .bindPopup(`Départ - ${locations[0].street}`)
+        L.marker([locations[1].latLng.lat, locations[1].latLng.lng])
+          .addTo(pathMapQuest)
+          .bindPopup(`End - ${locations[1].street}`)
+
+        L.polyline(latlngs, { color: "red" }).addTo(pathMapQuest)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  })
